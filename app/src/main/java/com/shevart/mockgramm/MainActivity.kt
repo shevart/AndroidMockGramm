956d36363e4ada75959a91b82cda2e37dd12c63f
Artem Shevchenko <shevartsoft@gmail.com>
2018-08-24 14:18:56 +0300

    camera - work with main screen UI

--- app/src/main/java/com/shevart/mockgramm/MainActivity.kt
+++ app/src/main/java/com/shevart/mockgramm/MainActivity.kt
@@ -1,16 +1,18 @@
 package com.shevart.mockgramm
 
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.shevart.mockgramm.base.BaseActivity
 import com.shevart.mockgramm.test.camera.TestCameraActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
+    override fun provideLayoutResId() = R.layout.activity_main
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        actionBar?.hide()
 
+        // todo test only code
         startActivity(Intent(this, TestCameraActivity::class.java))
     }
 }
