7291e1269f9091383a8937aeb5ead658db7b0e6c
Artem Shevchenko <shevartsoft@gmail.com>
2018-08-25 13:59:04 +0300

    editphoto - work with edit photo section, add BundleUtil file, start work with display photo on the edit screen

--- app/src/main/java/com/shevart/mockgramm/screens/MainActivity.kt
+++ app/src/main/java/com/shevart/mockgramm/screens/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.shevart.mockgramm.screens
 
+import android.net.Uri
 import android.os.Bundle
 import com.shevart.mockgramm.R
 import com.shevart.mockgramm.base.BaseActivity
 import com.shevart.mockgramm.screens.camera.CameraFragment
+import com.shevart.mockgramm.screens.editphoto.EditPhotoFragment
 
 class MainActivity : BaseActivity() {
     override fun provideLayoutResId() = R.layout.activity_main
@@ -12,10 +14,21 @@ class MainActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
 
         if (supportFragmentManager.findFragmentById(R.id.flContainer) == null) {
-            supportFragmentManager
-                    .beginTransaction()
-                    .add(R.id.flContainer, CameraFragment.getInstance())
-                    .commit()
+            showCameraScreen()
         }
+    }
+
+    private fun showCameraScreen() {
+        supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.flContainer, CameraFragment.getInstance())
+                .commit()
+    }
+
+    private fun showEditPhotoScreen(photoUri: Uri) {
+        supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.flContainer, EditPhotoFragment.getInstance(photoUri))
+                .commit()
     }
 }
--- app/src/main/java/com/shevart/mockgramm/screens/editphoto/EditPhotoFragment.kt
+++ app/src/main/java/com/shevart/mockgramm/screens/editphoto/EditPhotoFragment.kt
@@ -1,15 +1,38 @@
 package com.shevart.mockgramm.screens.editphoto
 
+import android.net.Uri
+import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
 import com.shevart.mockgramm.R
 import com.shevart.mockgramm.base.BaseFragment
+import com.shevart.mockgramm.util.getPhotoUri
+import com.shevart.mockgramm.util.setPhotoUri
+import kotlinx.android.synthetic.main.fragment_edit_photo.*
 
+@Suppress("unused")
 class EditPhotoFragment : BaseFragment() {
+    private val photoUri: Uri
+        get() = arguments?.getPhotoUri()
+                ?: throw IllegalArgumentException("You must pass photoUri as argument!")
+
     override fun provideLayoutResId() = R.layout.fragment_edit_photo
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val bitmap = MediaStore.Images.Media.getBitmap(forceContext.contentResolver, photoUri)
+        if (bitmap != null) {
+            ivEditedPhoto.setImageBitmap(bitmap)
+        }
+    }
+
     companion object {
-        // todo pass here image URI
-        fun getInstance(): EditPhotoFragment {
-            return getInstance()
+        fun getInstance(photoUri: Uri): EditPhotoFragment {
+            return EditPhotoFragment().apply {
+                arguments = Bundle(1)
+                        .setPhotoUri(photoUri)
+            }
         }
     }
 }
